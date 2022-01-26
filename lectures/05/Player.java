a008d07dfe3518291e314e886a1dd9fa6a2e0e86
c212 <40395648+c212@users.noreply.github.com>
2022-01-26 17:48:46 -0500

    now with partial order

--- lectures/05/Player.java
+++ lectures/05/Player.java
@@ -2,15 +2,17 @@ import java.util.Arrays;
 
 public class Player implements Comparable<Player> {
     public int compareTo(Player another) {
-        if (this.points  > 21 && another.points  > 21) return this.points - another.points;
-        if (this.points <= 21 && another.points <= 21) return -(this.points - another.points);
-        if (this.points <= 21 && another.points  > 21) return -1;
-        else // if (this.points  > 21 && another.points <= 21) 
-          return 1;
-        
+      // n1 <= n2 iff n2 % n1 == 0 (n1 factor of n2) 
+      if (this.points % another.points == 0) return 1;
+      if (another.points % this.points == 0) return -1; 
+      else { 
+          System.out.println("Can't compare " + this + " with " + another);
+          // System.exit(0); 
+          return 0; // but how could we throw in fact an exception
+      }
     }
     String name;
-    int points;
+    int points; // > 0 
     public Player(String name, int points) {
         this.name = name;
         this.points = points;
@@ -20,13 +22,17 @@ public class Player implements Comparable<Player> {
     }
     public static void main(String[] args) {
         Player[] players = new Player[5];
-        players[0] = new Player("Suraj", 13); 
-        players[1] = new Player("Spence", 32); 
-        players[2] = new Player("Garrett", 19); 
-        players[3] = new Player("Elliot", 21); 
-        players[4] = new Player("Jane", 25); 
+        players[0] = new Player("Suraj", 128); 
+        players[1] = new Player("Spence", 9); 
+        players[2] = new Player("Garrett", 16); 
+        players[3] = new Player("Elliot", 3); 
+        players[4] = new Player("Jane", 8); 
         System.out.println(Arrays.toString(players));
-        Arrays.sort(players); 
-        System.out.println(Arrays.toString(players));
+        try { 
+            Arrays.sort(players); 
+            System.out.println(Arrays.toString(players));
+        } catch (Exception e) {
+            System.out.println("This ordering is partial.");    
+        }
     }
 }
